b4c9158d63f8347bd7d000e8a8e919bb5534a83e
Cip <xeonsat@gmail.com>
2021-04-20 14:18:31 +0800

    my library

---
+++ jsHTML/myLibrary.js
@@ -0,0 +1,23 @@
+/**
+ * Use template literals to output HTML
+ */
+import Books from "./Books.js";
+
+const bookOne = new Books(
+    "Francis Fukuyama",
+    "The Origins of Political Order: From Prehuman Times to the French Revolution"
+);
+
+const content = `
+  <main>
+    <article>
+      <ul>
+        <li>Author: ${bookOne.author}</li>
+        <li>Title: ${bookOne.title}</li>
+      </ul>
+    </article>
+  </main>
+`;
+
+document.body.innerHTML = content;
+
